323e5f720860baf60ce4fc917e534ab3a37dc3a3
joaquinquiroga22 <joaquinquiroga22@gmail>
2022-08-11 15:10:00 -0300

    first commit

---
+++ Ejercicios.js
@@ -0,0 +1,36 @@
+function Proto() {
+    // Crea un objeto
+    // Luego crea otro objeto en donde el sea su padre y ademas se cree como nuevo objeto
+    // Pista, utilizar: __proto__
+    // Tu código:
+}
+function agregarMetodoPrototype(Constructor) {
+    // Agrega un método al Constructor del `prototype`
+    // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
+    // Tu código:
+
+}
+
+function agregarStringInvertida() {
+    // Agrega un método al prototype de String que devuelva la misma cadena de caracteres, pero invertida.
+    // El método debe llamarse "reverse"
+    // Ej: 'menem'.reverse() => menem
+    // 'joaco'.reverse() => 'ocaoj' 
+    // Pista: Necesitarás usar "this" dentro de "reverse"
+
+}
+
+function changeNombre() {
+    // Crea un objeto
+    // Agregar un getNombre()
+    // Agregar un changeNombre()
+    // Mostrar por consola changeNombre() y getNombre(), con nombre distinto en changeNombre() al del constructor y al del getNombre()
+    // Tu código:
+}
+
+function Object() {
+    // Crea un objeto
+    // Luego crea otro objeto en donde el sea su padre y ademas se cree como nuevo objeto
+    // Utilizar obligatoriamente el Metodo Object
+    // Tu código:
+}
